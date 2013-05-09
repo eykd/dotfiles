dc6a5157fe578fcb5be7188444a8220384f5c8cc
deyk <deyk@crossway.org>
2013-05-09 10:19:48 -0700

    Added Aquamacs settings.

---
+++ LibraryPreferencesAquamacs/Preferences.el
@@ -0,0 +1,11 @@
+;; This is the Aquamacs Preferences file.
+;; Add Emacs-Lisp code here that should be executed whenever
+;; you start Aquamacs Emacs. If errors occur, Aquamacs will stop
+;; evaluating this file and print errors in the *Messags* buffer.
+;; Use this file in place of ~/.emacs (which is loaded as well.)
+
+(server-start)
+
+;; Settings
+(tool-bar-mode -1)
+(set-fringe-style t)
---
+++ LibraryPreferencesAquamacs/customizations.el
@@ -0,0 +1,38 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 215 t)
+ '(aquamacs-tool-bar-user-customization (quote ((16777249 new-file open-file recent-files save-buffer aquamacs-print nil undo redo cut copy paste isearch-forward nil customize help))) t)
+ '(auto-fill-function nil t)
+ '(default-frame-alist (quote ((right-fringe . 0) (left-fringe . 4) (fringe) (internal-border-width . 0) (vertical-scroll-bars . right) (cursor-type . box) (menu-bar-lines . 1) (background-color . "#2e3436") (background-mode . dark) (border-color . "#888a85") (cursor-color . "#fce94f") (foreground-color . "#eeeeec") (mouse-color . "#8ae234"))))
+ '(fill-column 80)
+ '(fringe-indicator-alist (quote ((continuation nil nil) (truncation left-truncation right-truncation) (continuation left-continuation right-continuation) (overlay-arrow . right-triangle) (up . up-arrow) (down . down-arrow) (top top-left-angle top-right-angle) (bottom bottom-left-angle bottom-right-angle top-right-angle top-left-angle) (top-bottom left-bracket right-bracket top-right-angle top-left-angle) (empty-line . empty-line) (unknown . question-mark))) t)
+ '(global-hl-line-mode t)
+ '(global-linum-mode t)
+ '(ido-enable-tramp-completion nil)
+ '(ido-everywhere t)
+ '(ido-use-filename-at-point nil)
+ '(indent-tabs-mode nil)
+ '(indicate-buffer-boundaries (quote left))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote small) t)
+ '(py-closing-list-dedents-bos t)
+ '(py-electric-comment-add-space-p t)
+ '(py-electric-comment-p t)
+ '(py-load-pymacs-p t)
+ '(py-prepare-autopair-mode-p t)
+ '(py-set-fill-column-p t)
+ '(py-tab-indents-region-p t)
+ '(py-tab-shifts-region-p t)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(rst-mode-default ((t (:inherit text-mode-default :height 130 :family "Menlo"))) t)
+ '(text-mode-default ((t (:inherit autoface-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 130 :width normal :family "Menlo")))))
