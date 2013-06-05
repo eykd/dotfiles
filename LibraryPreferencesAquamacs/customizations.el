012d0cbd4ba01c0fa25b55e92d2c0608d2250b4c
deyk <deyk@crossway.org>
2013-06-05 11:06:33 -0700

    More customization tweaks.

--- LibraryPreferencesAquamacs/customizations.el
+++ LibraryPreferencesAquamacs/customizations.el
@@ -7,6 +7,7 @@
  '(aquamacs-customization-version-id 215 t)
  '(aquamacs-tool-bar-user-customization (quote ((16777249 new-file open-file recent-files save-buffer aquamacs-print nil undo redo cut copy paste isearch-forward nil customize help))) t)
  '(auto-fill-function nil t)
+ '(auto-word-wrap-default-function nil)
  '(default-frame-alist (quote ((right-fringe . 0) (left-fringe . 4) (fringe) (internal-border-width . 0) (vertical-scroll-bars . right) (cursor-type . box) (menu-bar-lines . 1) (background-color . "#2e3436") (background-mode . dark) (border-color . "#888a85") (cursor-color . "#fce94f") (foreground-color . "#eeeeec") (mouse-color . "#8ae234"))))
  '(fill-column 80)
  '(fringe-indicator-alist (quote ((continuation nil nil) (continuation nil nil) (truncation left-truncation right-truncation) (continuation left-continuation right-continuation) (overlay-arrow . right-triangle) (up . up-arrow) (down . down-arrow) (top top-left-angle top-right-angle) (bottom bottom-left-angle bottom-right-angle top-right-angle top-left-angle) (top-bottom left-bracket right-bracket top-right-angle top-left-angle) (empty-line . empty-line) (unknown . question-mark))) t)
@@ -17,6 +18,8 @@
  '(ido-use-filename-at-point nil)
  '(indent-tabs-mode nil)
  '(indicate-buffer-boundaries (quote left))
+ '(markdown-follow-wiki-link-on-enter nil)
+ '(nil nil t)
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode (quote small) t)
  '(py-closing-list-dedents-bos t)
@@ -26,12 +29,11 @@
  '(py-tab-indents-region-p t)
  '(rst-adornment-faces-alist (quote ((t . font-lock-keyword-face) (nil . font-lock-keyword-face) (5 . rst-level-5-face) (6 . rst-level-6-face))))
  '(uniquify-buffer-name-style (quote forward) nil (uniquify))
- '(visual-line-mode t t)
-)
+ '(visual-line-mode t t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(rst-mode-default ((t (:inherit text-mode-default :height 130 :family "Menlo"))) t)
- '(text-mode-default ((t (:inherit autoface-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 130 :width normal :family "Menlo")))))
+ '(markdown-mode-default ((t (:inherit text-mode-default :height 140 :family "Consolas"))) t)
+ '(rst-mode-default ((t (:inherit text-mode-default :height 130 :family "Menlo"))) t))
