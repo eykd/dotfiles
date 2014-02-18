85edc248a199bdcc4fbbae8211145e87703f1faa
deyk <deyk@crossway.org>
2014-02-17 17:02:17 -0800

    Customization updates.

--- LibraryPreferencesAquamacs/customizations.el
+++ LibraryPreferencesAquamacs/customizations.el
@@ -4,28 +4,27 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(aquamacs-additional-fontsets nil t)
- '(fill-column 80)
  '(aquamacs-customization-version-id 216 t)
  '(aquamacs-tool-bar-user-customization nil t)
- '(default-frame-alist (quote ((menu-bar-lines . 1) (foreground-color . "Black") (background-color . "White") (cursor-type . box) (cursor-color . "Red") (vertical-scroll-bars . right) (internal-border-width . 0) (left-fringe . 1) (right-fringe) (fringe) (tool-bar-lines . 0))))
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (fringe) (right-fringe) (left-fringe . 1) (internal-border-width . 0) (vertical-scroll-bars . right) (cursor-type . box) (menu-bar-lines . 1) (background-color . "#2e3436") (background-mode . dark) (border-color . "#888a85") (cursor-color . "#fce94f") (foreground-color . "#eeeeec") (mouse-color . "#8ae234"))))
+ '(fill-column 80)
  '(global-hl-line-mode t)
  '(global-linum-mode t)
  '(ido-enable-tramp-completion nil)
  '(ido-everywhere t)
  '(ido-use-filename-at-point nil)
+ '(indent-tabs-mode nil)
  '(indicate-buffer-boundaries (quote left))
  '(markdown-follow-wiki-link-on-enter nil)
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
  '(rst-adornment-faces-alist (quote ((t . font-lock-keyword-face) (nil . font-lock-keyword-face) (5 . rst-level-5-face) (6 . rst-level-6-face))))
  '(size-indication-mode t)
  '(uniquify-buffer-name-style (quote forward) nil (uniquify))
  '(visual-line-mode t t))
- '(ns-tool-bar-display-mode nil t)
- '(ns-tool-bar-size-mode nil t)
- '(visual-line-mode nil t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(markdown-mode-default ((t (:inherit text-mode-default :height 140 :family "Consolas"))) t)
- '(rst-mode-default ((t (:inherit text-mode-default :height 130 :family "Menlo"))) t))
+ '(rst-mode-default ((t (:inherit text-mode-default :height 160 :family "Consolas"))) t))
